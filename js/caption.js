ed23003b5c56845a12e5b7548c6cb83f9fa2a056
AcVoyager <3150104927@zju.edu.cn>
2018-05-13 22:29:21 +0800

    vs 0.9.3

--- js/caption.js
+++ js/caption.js
@@ -1,7 +1,7 @@
 
-var myurl=document.getElementById("urlInput");
-var urlbtn=document.getElementById("urlBtn");
-var caprst=document.getElementById("captionresult");
+var myurl = document.getElementById("urlInput");
+var urlbtn = document.getElementById("urlBtn");
+var caprst = document.getElementById("captionresult");
 
 //给后端请求ajax
 //返回 JSON
@@ -10,39 +10,28 @@ alert(111);
 //caprst.innerHTML = String(caprst);
 
 //alert($('#urlInput'));
-//$(document).ready(function(){
-    //console.log(333);
-    //alert(112);
-//alert($("[id='urlInput']").val());
-$(function(){    
-$("[id='urlBtn']").click(function(){
-    //alert(222);
-    alert($("[id='urlInput']").val());
-    htmlobj=$.ajax({
-        type:'POST',
-        //url:"/jquery/test1.txt",
-        url:"develop/AjaxTest.php",//服务器
-        data: {
-            "myurl" : $("[id='urlInput']").val()
-        },
-        success: function(data){
-            alert(333);
-            var dataObj = JSON.parse(data);
-            //console.log(data);
-            //data.prc_url;
-            alert(444);
-            alert(dataObj.tags);
-            var html1='<div class="img lazy-img" style="padding-bottom: 60.10738255033557%;"><img data-src=String('+dataObj.prc_url+') alt=""></div>';
-            $("[id='imgout']").append(html1);
-            var html2='<p class="indents-2" id="captionresult">'+dataObj.pic_caption+dataObj.src_text+dataObj.tags;+'</p>';
-            $("[id='captionresult']").append(html2);
-            //data.src_text;
-            //data.tags;
-        }
+$(document).ready(function () {
+    $("[id='urlBtn']").click(function () {
+        //alert(222);
+        alert($("[id='urlInput']").val());
+        $.post(
+            'develop/AjaxTest.php',
+            {
+                "myurl": $("[id='urlInput']").val()
+            },
+            function (data) {
+                alert(333);
+                var dataObj = JSON.parse(data);
+                alert(444);
+                alert(dataObj.tags);
+                var html1 = '<div class="img lazy-img" style="padding-bottom: 60.10738255033557%;"><img data-src=String(' + dataObj.prc_url + ') alt=""></div>';
+                $("[id='imgout']").append(html1);
+                var html2 = '<p class="indents-2" id="captionresult">' + dataObj.pic_caption + dataObj.src_text + dataObj.tags; +'</p>';
+                $("[id='captionresult']").append(html2);
+            }
+        );
     });
-    });
-})
-//});
+});
 
 //请求myurl
 
